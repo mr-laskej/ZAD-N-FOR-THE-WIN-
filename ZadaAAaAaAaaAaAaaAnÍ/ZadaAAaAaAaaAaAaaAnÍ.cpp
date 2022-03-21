cef8cb0b062d72409b8fbadbe79a38af1c37ff1d
Matyáš Laský <matyas.lasky.020@pslib.cz>
2022-03-21 13:27:31 +0100

    Přidejte soubory projektu.

---
+++ ZadaAAaAaAaaAaAaaAnÍ/ZadaAAaAaAaaAaAaaAnÍ.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main(void) 
+{
+	int i = 0;
+	int sudy = 0;
+	int lichy = 0;
+	printf("Napiste libovolne cislo: \n");
+	scanf("%d", &i);
+	for (int j = 0; j <= i; j++) 
+	{
+		if (j % 2 == 0) {
+			sudy += j;
+		}
+		else (j % 2 != 0); {
+			lichy += j;
+		}
+
+	}
+	printf("soucet sudych cisel je: %d\n", sudy);
+	printf("soucet lichych cisel je: %d\n", lichy);
+}
+
